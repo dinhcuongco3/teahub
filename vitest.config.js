4cb58785991e7b4c3f62bbd70629a4102e260352
twoody <tanner.l.woody@gmail.com>
2023-04-24 22:56:33 -0700

    a guide ...

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,13 @@
-export default {
-  // The folder where your tests are located
-  testDir: 'tests',
+import { defineConfig } from 'vitest/config'
+import Vue from "@vitejs/plugin-vue";
 
-  // The file extension of your test files
-  testMatch: ['**/*.test.{js,jsx,ts,tsx}'],
-};
+export default defineConfig({
+	plugins: [Vue()],
+	root: ".", //Define the root
+	test: {
+		globals: true,
+		environment: "jsdom",
+		// The file extension of your test files
+		include: ['tests/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+	}
+})
