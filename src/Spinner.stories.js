b16f4ebb10d362ce53ca84b4f4b08764bdffbe48
twoody <tanner.l.woody@gmail.com>
2023-05-04 13:04:20 -0700

    lint

--- src/Spinner.stories.js
+++ src/Spinner.stories.js
@@ -1,20 +1,27 @@
-import Spinner from "@/components/common/loading/Spinner.vue";
-import "@/assets/styles/styles.less";
+import Spinner from "@/components/common/loading/Spinner.vue"
+import "@/assets/styles/styles.less"
 
 export default {
   title: "Components/Common/Loading/Spinner",
   component: Spinner,
-  tags: ["autodocs"],
-};
+  tags: [
+    "autodocs",
+  ],
+}
 
 const Template = (args) => ({
-  components: { Spinner },
-  setup() {
-    return { args };
+  components: {
+    Spinner, 
   },
-  template: '<spinner v-bind="args" />',
-});
+  setup () 
+  {
+    return {
+      args, 
+    }
+  },
+  template: "<spinner v-bind=\"args\" />",
+})
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default = Template.bind({})
+Default.args = {}
 
